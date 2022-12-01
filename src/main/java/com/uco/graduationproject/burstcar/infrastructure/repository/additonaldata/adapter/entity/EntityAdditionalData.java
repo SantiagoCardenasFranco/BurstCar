0cc66793f2f33c80bd23aba10e17e600400be99e
CEIBA\santiago.cardenas <santiago.cardenas@ceiba.com.co>
2022-12-01 08:13:44 -0500

    Add tests of integration, implemetation CI with github actions and test jmeter

--- src/main/java/com/uco/graduationproject/burstcar/infrastructure/repository/additonaldata/adapter/entity/EntityAdditionalData.java
+++ src/main/java/com/uco/graduationproject/burstcar/infrastructure/repository/additonaldata/adapter/entity/EntityAdditionalData.java
@@ -1,15 +1,12 @@
 package com.uco.graduationproject.burstcar.infrastructure.repository.additonaldata.adapter.entity;
 
 import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Getter
-@Setter
 @Entity
 @Table(name = "additional_data")
 public class EntityAdditionalData {
